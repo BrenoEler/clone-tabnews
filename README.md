e72487bf6c3c3348a0f0f0e0724ebd13e6ce247d
Breno Eler <brenodesouzaeler@gmail.com>
2026-02-26 09:35:17 -0300

    Adiciona script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositorio dedicado a estudos
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-
 function Home() {
-  return <h1>Julia, eu amo você. Se você me ama, dá uma risadinha! ❤️</h1>
+  return <h1>Julia, eu amo você. Se você me ama, dá uma risadinha! ❤️</h1>;
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste() {
+  console.log("Indentação erradaaaaaaaa");
 }
 
 export default Home;
