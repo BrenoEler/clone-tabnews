1532472489de09fe399190368319dadb776e3b6e
Breno Eler <brenodesouzaeler@gmail.com>
2026-02-26 09:38:13 -0300

    remove funções desnecessária

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Julia, eu amo você. Se você me ama, dá uma risadinha! ❤️</h1>;
 }
 
-function teste() {
-  console.log("Teste");
-}
-
-function teste() {
-  console.log("Indentação erradaaaaaaaa");
-}
-
 export default Home;
